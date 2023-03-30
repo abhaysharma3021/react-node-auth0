fb8cc4f4a75738ae17c6301f1d2d7c982f9b4113
Abhay Sharma <abhay.sharma.3021@gmail.com>
2023-03-30 11:47:53 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.listen(8080, () => console.log(`Server running on PORT:8080`));
